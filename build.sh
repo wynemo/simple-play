d858771ab14ade4d4ed85382683ce65a847bb168
lozhang <lozhang@noreply.gitcode.com>
2025-10-22 17:38:43 +0800

    注释掉构建脚本中的 Docker 推送命令

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ PLATFORM="${PLATFORM:-linux/amd64}"
 
 
 docker build --platform $PLATFORM -t $IMAGE_NAME .
-docker push $IMAGE_NAME
+# docker push $IMAGE_NAME
 
 # docker run --env-file setting.env $IMAGE_NAME
