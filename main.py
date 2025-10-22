87fc2205bb3d8605f198763eae63198d92d45b97
lozhang <lozhang@noreply.gitcode.com>
2025-10-22 16:52:42 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+from fastapi import FastAPI
+from fastapi_toolbox import run_server, logger
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    logger.info("Hello World访问")
+    return {"Hello": "World"}
+
+
 def main():
     print("Hello from simple-play!")
 
 
 if __name__ == "__main__":
-    main()
+    run_server(
+        "main:app",
+        log_file="logs/app.log",
+    )
